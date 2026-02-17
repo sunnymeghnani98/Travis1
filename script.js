6e924a574b97ac9a235cf72bd1914be572845025
sunnymeghnani98 <meghnani1998@gmail.com>
2026-02-17 11:35:58 +0530

    added subtraction feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const num1Input = document.getElementById('num1');
 const num2Input = document.getElementById('num2');
 const addBtn = document.getElementById('addBtn');
+const subBtn = document.getElementById('subBtn');
 const result = document.getElementById('result');
 
 addBtn.addEventListener('click', () => {
@@ -11,5 +12,16 @@ addBtn.addEventListener('click', () => {
         result.textContent = 'Please enter valid numbers';
     } else {
         result.textContent = `Result: ${num1 + num2}`;
+    }
+});
+
+subBtn.addEventListener('click', () => {
+    const num1 = parseFloat(num1Input.value);
+    const num2 = parseFloat(num2Input.value);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        result.textContent = 'Please enter valid numbers';
+    } else {
+        result.textContent = `Result: ${num1 - num2}`;
     }
 });
